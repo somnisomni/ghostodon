f202bafd89e9c77736dfe63574e01ad81bbc89c7
somni <me@somni.one>
2023-02-06 00:48:27 +0900

    Fix form data not appending data

--- src/mastodon/api.ts
+++ src/mastodon/api.ts
@@ -2,8 +2,8 @@ import config from "../../config/config.json";
 
 export async function createStatus(status: string, visibility: "public" | "unlisted" | "private" | "direct" = "public"): Promise<Record<string, unknown>> {
   const formData = new FormData();
-  formData.append("status", status);
-  formData.append("visibility", visibility);
+  formData.set("status", status);
+  formData.set("visibility", visibility);
 
   const response = await fetch(`https://${config.mastodon.instanceHost}/api/v1/statuses`, {
     method: "POST",
