dd1423599039bfda379deb6eaa62ad363fedabf6
somni <me@somni.one>
2023-02-05 23:52:11 +0900

    Give up using file handle

--- src/uuid-cache.ts
+++ src/uuid-cache.ts
@@ -3,37 +3,21 @@ import { UUIDCache } from "./interface/app";
 import { POST_UUID_CACHE_FILE_PATH } from "./constants";
 
 export default class UUIDCacheManager {
-  private static cacheFileHandle: fs.FileHandle;
-
   static async init(): Promise<void> {
     // Create cache file if not exist
     try {
       await fs.writeFile(POST_UUID_CACHE_FILE_PATH, "{}", { encoding: "utf8", flag: "wx", mode: 0o644 });
     } catch { /* Nothing */ }
-
-    // Open cache file handle
-    try {
-      this.cacheFileHandle = await fs.open(POST_UUID_CACHE_FILE_PATH, "r+");
-    } catch(error) {
-      console.error("Can't create or open UUID cache file!");
-      console.error(error);
-    }
-  }
-
-  static async close(): Promise<void> {
-    this.cacheFileHandle.close();
   }
 
   private static async readUUIDCache(): Promise<UUIDCache> {
-    const content: UUIDCache = JSON.parse(await this.cacheFileHandle.readFile({ encoding: "utf8" }));
+    const content: UUIDCache = JSON.parse(await fs.readFile(POST_UUID_CACHE_FILE_PATH, { encoding: "utf8" }));
     return content;
   }
 
   private static async writeUUIDCache(uuidCache: UUIDCache): Promise<boolean> {
     try {
-      await this.cacheFileHandle.truncate();
-      await this.cacheFileHandle.writeFile(JSON.stringify(uuidCache), { encoding: "utf8" });
-      await this.cacheFileHandle.sync();
+      await fs.writeFile(POST_UUID_CACHE_FILE_PATH, JSON.stringify(uuidCache), { encoding: "utf8" });
       return true;
     } catch {
       return false;
