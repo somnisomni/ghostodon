9241c4bf0ae3dddde382282a219a4a861f539f5b
somni <me@somni.one>
2023-02-06 00:20:15 +0900

    More logging & UUID check in Webhook routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import fastify, { FastifyLoggerOptions } from "fastify";
+import fastify, { FastifyLoggerOptions, FastifyReply, FastifyRequest } from "fastify";
 import { FastifyInstance } from "fastify";
-import { GhostWebhookPost } from "./interface/ghost";
+import { GhostPost, GhostWebhookPost } from "./interface/ghost";
 import { SERVER_LOG_FILE_PATH } from "./constants";
 import UUIDCacheManager from "./uuid-cache";
 import config from "../config/config.json";
@@ -24,6 +24,18 @@ export default class WebhookListener {
   }
 
   private setup() {
+    const commonWebhookRouteHandler = (request: FastifyRequest, reply: FastifyReply): GhostPost | null => {
+      console.info(`${request.method.toUpperCase()} ${request.routerPath}`);
+
+      if(!("post" in (request.body as Record<string, unknown>))) {
+        console.warn("  └ No 'post' property in request body; is it valid Webhook request from Ghost?");
+        reply.code(500).send("ERROR");
+        return null;
+      }
+
+      return (request.body as GhostWebhookPost).post.current;
+    };
+
     if(this.server) {
       // Default handler
       this.server.setNotFoundHandler((_, reply) => {
@@ -36,25 +48,40 @@ export default class WebhookListener {
 
       // Ping
       this.server.get("/ping", (_, reply) => {
-        reply.send("ALIVE");
+        reply.code(200).send("ALIVE");
       });
 
       // Ghost new post published → Mastodon new status
-      this.server.post("/post/new", (request, reply) => {
-        const body = (request.body as GhostWebhookPost).post.current;
-        console.log(`PUBLISHED: ${body.title} (${body.url})`);
+      this.server.post("/post/new", async (request, reply) => {
+        const body = commonWebhookRouteHandler(request, reply);
+        if(!body) return;
+        console.log(`  └ Webhook parsed, "${body.title}" (${body.url})`);
 
         // Once published, uuid of the post should be stored to prevent recreate status after unpublish-publish
-        UUIDCacheManager.cacheUUID(body.uuid);
+        if(await UUIDCacheManager.hasUUID(body.uuid)) {
+          console.log(`  └ UUID ${body.uuid} is already published at least once. Nothing to do.`);
+          reply.code(200).send("OK, DUPLICATED");
+          return;
+        }
+
+        if(!config.bridge.newStatus.onPostPublished) {
+          console.log("  └ Creating new status on post publish is disabled. Nothing to do.");
+          reply.code(200).send("OK, NOT_SHARED");
+          return;
+        }
+
+        // Mastodon API should be here
+        console.log(`  └ Caching UUID ${body.uuid}.`);
+        await UUIDCacheManager.cacheUUID(body.uuid);
 
         reply.code(200).send("OK");
       });
 
       // Ghost post update → Mastodon new status
       this.server.post("/post/update", (request, reply) => {
-        // update
-        const body = (request.body as GhostWebhookPost).post.current;
-        console.log(`UPDATED: ${body.title} (${body.url})`);
+        const body = commonWebhookRouteHandler(request, reply);
+        if(!body) return;
+        console.log(`  └ Webhook parsed, "${body.title}" (${body.url})`);
 
         reply.code(200).send("OK");
       });
