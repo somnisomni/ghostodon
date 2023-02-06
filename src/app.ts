70bb303c4ef0c6c9262b028dccf21e5ba715e279
somni <me@somni.one>
2023-02-06 17:40:23 +0900

    Create app initialization function

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
+import * as fs from "fs/promises";
 import WebhookListener from "./server";
 import UUIDCacheManager from "./util/uuid-cache";
+import { CONFIG_FOLDER_PATH, LOG_FOLDER_PATH } from "./util/constants";
 
-UUIDCacheManager.init();
+async function appInit() {
+  await fs.mkdir(CONFIG_FOLDER_PATH, { recursive: true });
+  await fs.mkdir(LOG_FOLDER_PATH, { recursive: true });
+}
 
-const webhookListener = new WebhookListener();
-webhookListener.listen();
+async function main() {
+  await appInit();
+  await UUIDCacheManager.init();
+
+  const webhookListener = new WebhookListener();
+  webhookListener.listen();
+}
+
+main();
